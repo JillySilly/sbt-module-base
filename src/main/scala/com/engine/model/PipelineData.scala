5c25948bcb907fff9fda50e342924ab83fb84558
JillySilly <ranaebaddiefb@gmail.com>
2026-08-01 03:56:39 +0000

    chore: sync sbt module dataset schema definitions

--- src/main/scala/com/engine/model/PipelineData.scala
+++ src/main/scala/com/engine/model/PipelineData.scala
@@ -7,3 +7,9 @@ final case class ClusterVector_7309(
   nodeId: Long = 11317L,
   active: Boolean = true
 )
+
+/** Pipeline node state representation 22218 */
+final case class StateRecord_12201(
+  nodeId: Long = 16491L,
+  active: Boolean = true
+)
