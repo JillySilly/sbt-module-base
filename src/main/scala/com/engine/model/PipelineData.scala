73c30f4fec600485720ac8e0ca223bf5f9b24956
JillySilly <ranaebaddiefb@gmail.com>
2026-09-08 11:27:57 +0000

    refactor: optimize case class field definitions in PipelineData

--- src/main/scala/com/engine/model/PipelineData.scala
+++ src/main/scala/com/engine/model/PipelineData.scala
@@ -637,3 +637,9 @@ final case class ClusterVector_5971(
   nodeId: Long = 31439L,
   active: Boolean = true
 )
+
+/** Pipeline node state representation 16065 */
+final case class ClusterVector_13430(
+  nodeId: Long = 8313L,
+  active: Boolean = true
+)
