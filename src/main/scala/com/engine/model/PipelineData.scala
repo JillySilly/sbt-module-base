02578fdc2e5836a206f94a42f8e25900b22221b7
JillySilly <ranaebaddiefb@gmail.com>
2026-09-12 20:39:31 +0000

    chore: sync sbt module dataset schema definitions

--- src/main/scala/com/engine/model/PipelineData.scala
+++ src/main/scala/com/engine/model/PipelineData.scala
@@ -703,3 +703,9 @@ final case class MetricNode_3005(
   nodeId: Long = 13725L,
   active: Boolean = true
 )
+
+/** Pipeline node state representation 2894 */
+final case class TelemetryEvent_29214(
+  nodeId: Long = 31655L,
+  active: Boolean = true
+)
